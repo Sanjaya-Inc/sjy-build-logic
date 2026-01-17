77cb1e580af545f321a5c7d548114d20d0857eec
Joseph Sanjaya <sanjayajosep@gmail.com>
2026-01-17 14:30:46 +0700

    refactor(core): restructure data and design system modules
    This commit restructures the project's core modules by:
    - Renaming the `core:supabase` module to `core:data-supabase` for clarity.
    - Introducing new data layer modules: `core:data-local` and `core:data-pref`.
    - Creating a new `core:design-system` module for UI components.
    - Updating `settings.gradle.kts` and `composeApp/build.gradle.kts` to reflect these module changes.
    - Adding the `datastore-preferences` dependency.

---
+++ shared/multiplatform/data-local/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(sjy.plugins.buildlogic.multiplatform.lib)
+    alias(sjy.plugins.buildconfig.kmp)
+}
+
+kotlin {
+    android {
+        namespace = "core.data.local"
+    }
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.utils)
+            implementation(project.dependencies.platform(sjy.supabase.bom))
+            implementation(sjy.bundles.supabase.core)
+        }
+    }
+}
+
+ktorfit {
+    compilerPluginVersion.set("2.3.3")
+}
