1a318b7edfba0acaeca16d00ba8d6fc7655fcf03
Joseph Sanjaya <sanjayajosep@gmail.com>
2026-03-01 15:54:09 +0700

    feat: add flatMap extension for Result
    Adds an inline `flatMap` extension function to the `Result` type in the `core.utils` package to facilitate functional error handling and chaining.

---
+++ shared/multiplatform/utils/src/commonMain/kotlin/core/utils/ResultExt.kt
@@ -0,0 +1,8 @@
+package core.utils
+
+inline fun <T, R> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> {
+    return fold(
+        onSuccess = { transform(it) },
+        onFailure = { Result.failure(it) }
+    )
+}
